464d9e63a850be282eaa5f290054b0bd0ab8b8f8
Guillermo Cacho Valdez <guillermo.cacho.v@gmail.com>
2022-03-24 11:32:57 -0700

    Finally en excepciones agregado

--- Taller-NextClass/T9_excepciones.py
+++ Taller-NextClass/T9_excepciones.py
@@ -2,5 +2,8 @@ while True:
     try:
         edad = int(input("Ingrese su edad: "))
         print("Tu edad es: ", edad)
+        break
     except:
         print("Ingresaste un valor erroneo.")
+    finally:
+        print("La ejecución ha finalizado")
