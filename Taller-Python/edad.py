39758ef23347bf972d1a70acc9e6ffbf186b57d4
Guillermo Cacho Valdez <guillermo.cacho.v@gmail.com>
2022-03-31 19:00:51 -0700

    Reestructura de carpetas y nuevo archivo de POO en nextclass

--- Taller-Python/edad.py
+++
@@ -1,9 +0,0 @@
-def run():
-    edad = int(input("Tu edad= "))
-    contador = 1
-    while contador <= edad:
-        print(contador)
-        contador = contador + 1
-
-if __name__ == '__main__':
-    run()
