d0526358cb18843c5c69bce72b965b8b56ef6cb7
Guillermo Cacho Valdez <guillermo.cacho.v@gmail.com>
2022-04-07 16:52:04 -0600

    Nueva actualizacion en funcion de combate

--- Taller-NextClass/Programación estructurada/Ejercicios/4-funciones_combate.py
+++ Taller-NextClass/Programación estructurada/Ejercicios/4-funciones_combate.py
@@ -3,9 +3,9 @@ from time import sleep
 #----------------Diccionarios--------------------
 
 atacar = {
-    0 : "Fallido",
-    1 : "Basico",
-    2 : "Critico",
+    0 : "Falla el golpe",
+    1 : "Encesta un golpe Básico",
+    2 : "Encesta un golpe Crítico",
 }
 
 personaje = {
@@ -38,9 +38,16 @@ def nuevo_enemigo():
 def nuevo_ataque():
     nuevo_ataque = random.randint(0, 2)
     return atacar[nuevo_ataque]
+
+def estadisticas_enemigo(enemigo):
+    print(f"Aparece un {enemigo['tipo']} frente a ti.\nEl {enemigo['tipo']} tiene {enemigo['salud']} puntos de salud y su debilidad es una {enemigo['debilidad']}")
+
+def ataque_enemigo():
+    print(f"{enemigo['tipo']} te ataca y {nuevo_ataque()}")
     
 
 if __name__ == '__main__':
     enemigo = nuevo_enemigo()
-    print(f"Aparece un {enemigo} frente a ti. \n{enemigo} te ataca y te encesta un Golpe {nuevo_ataque()}")
+    estadisticas_enemigo(enemigo)
+    ataque_enemigo()
     
