1d44b6ad76cb0499c22553d675adecb2cbc08a35
Guillermo Cacho Valdez <guillermo.cacho.v@gmail.com>
2022-04-07 10:56:12 -0600

    Reestructura de carpetas y archivos en taller-nextclass

--- Taller-NextClass/Programación estructurada/T3_Condicionales.py
+++
@@ -1,8 +0,0 @@
-print("Ellige un vehículo:")
-select = int(input("Pulsa: 1 - Camioneta | 2 - Coche | 3 - Camion \n"))
-if select == 1:
-    print("Elegiste una camioneta")
-elif select == 2:
-    print("Elegiste un coche")
-elif select == 3:
-    print("Elegiste un camión")
--- Taller-Python/Programación Estructurada/18-Triangulo.py
+++
@@ -1,8 +0,0 @@
-def run():
-    num = int(input("Altura de la pirámide de asteriscos:\n----->")) #Obtiene la altura 
-
-    for i in range(num):
-        print(' ' * (num - i - 1) + "*" * (2 * i * 1) )
-
-if __name__ == '__main__':
-    run()
