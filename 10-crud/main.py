f7c18ce886c3c7e1b7cabd0d4def4d70cc7fa9b1
Guillermo Cacho <guillermo.cacho.v@gmail.com>
2022-06-06 18:56:18 -0600

    CRUD añadido y actualizado

--- 10-crud/main.py
+++ 10-crud/main.py
@@ -1,3 +1,5 @@
+import sys
+
 clients = 'pablo, ricardo, '
 
 
@@ -34,6 +36,15 @@ def delete_client(client_name):
         print('Client es not in clients list')
 
 
+def search_client(client_name):
+    clients_list = clients.split(',')
+    for client in clients_list:
+        if client != client_name:
+            continue
+        else: 
+            return True
+
+
 def _add_comma():
     global clients
 
@@ -47,10 +58,24 @@ def _print_welcome():
     print('[C]reate client')
     print('[D]elete client')
     print('[U]pdate client')
+    print('[S]earch clients')
 
 
 def _get_client_name():
-    return input('What is the client name? ')
+    client_name = None
+
+    while not client_name:
+        client_name = input('What is the client name? ')
+
+        if client_name == 'exit':
+            client_name = None
+            break
+
+        if not client_name:
+            pass
+           # sys.exit()
+
+    return client_name
 
 
 if __name__ == '__main__':
@@ -72,5 +97,12 @@ if __name__ == '__main__':
         updated_client_name = input('What is the updated client name \n')
         update_client(client_name, updated_client_name)
         list_clients()
+    elif command == 'S':
+        client_name = _get_client_name()
+        found = search_client(client_name)
+        if found:
+            print('The client es in the client\'s list')
+        else:
+            print('The client: {} is not in our client\'s list'.format(client_name))
     else:
         print('Invalid command')
